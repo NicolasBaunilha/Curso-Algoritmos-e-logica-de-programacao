1ae1d2e92584cfd065e94a16f07e066a47e0846c
Nicolas Barboza <nicolasbaunilha2@gmail.com>
2025-04-14 22:12:48 -0300

    Add files via upload

---
+++ Tarefas/Python_Exercicios/idades.py
@@ -0,0 +1,11 @@
+print("Dados da primeira pessoa: ")
+nome1 = str(input("Nome: "))
+idade1 = int(input("Idade: "))
+
+print("Dados da segunda pessoa: ")
+nome2 = str(input("Nome:"))
+idade2 = int(input("Idade: "))
+
+idadeMedia = (idade1 + idade2) / 2
+
+print("A idade media de", nome1, "e", nome2, "é de", idadeMedia, "anos")
